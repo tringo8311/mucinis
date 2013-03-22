2d396f97e355dc27f2305838fd72cf8fcd0bb434
tringo8311 <tringo8311@gmail.com>
2013-03-22 17:59:07 +0700

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,7 +7,7 @@
 
 // Initial Setup
 // ------------------------------------------------------------
-(function (a, b) {
+(function (a, b) {	
     var c = {
         thumb: ".thumb",
         slot: "li"
@@ -31,6 +31,123 @@
         })
     }
 })(jQuery);
+(function (a) {
+    a.fn.quicksand = function (b, c) {
+        var e = {
+            duration: 750,
+            easing: "swing",
+            attribute: "data-id",
+            adjustHeight: "auto",
+            useScaling: !0,
+            enhancement: function (a) {},
+            selector: "> *",
+            dx: 0,
+            dy: 0
+        };
+        a.extend(e, c);
+        if (a.browser.msie || typeof a.fn.scale == "undefined") e.useScaling = !1;
+        var f;
+        if (typeof arguments[1] == "function") var f = arguments[1];
+        else if (typeof (arguments[2] == "function")) var f = arguments[2];
+        return this.each(function (c) {
+            var g, h = [],
+                i = a(b).clone(),
+                j = a(this),
+                k = a(this).css("height"),
+                l, m = !1,
+                n = a(j).offset(),
+                o = [],
+                p = a(this).find(e.selector);
+            if (a.browser.msie && a.browser.version.substr(0, 1) < 7) j.html("").append(i);
+            else {
+                var q = 0,
+                    r = function () {
+                        q || (q = 1, $toDelete = j.find("> *"), j.prepend(w.find("> *")), $toDelete.remove(), m && j.css("height", l), e.enhancement(j), typeof f == "function" && f.call(this))
+                    },
+                    s = j.offsetParent(),
+                    t = s.offset();
+                s.css("position") == "relative" ? s.get(0).nodeName.toLowerCase() != "body" && (t.top += parseFloat(s.css("border-top-width")) || 0, t.left += parseFloat(s.css("border-left-width")) || 0) : (t.top -= parseFloat(s.css("border-top-width")) || 0, t.left -= parseFloat(s.css("border-left-width")) || 0, t.top -= parseFloat(s.css("margin-top")) || 0, t.left -= parseFloat(s.css("margin-left")) || 0), isNaN(t.left) && (t.left = 0), isNaN(t.top) && (t.top = 0), t.left -= e.dx, t.top -= e.dy, j.css("height", a(this).height()), p.each(function (b) {
+                    o[b] = a(this).offset()
+                }), a(this).stop();
+                var u = 0,
+                    v = 0;
+                p.each(function (b) {
+                    a(this).stop();
+                    var c = a(this).get(0);
+                    c.style.position == "absolute" ? (u = -e.dx, v = -e.dy) : (u = e.dx, v = e.dy), c.style.position = "absolute", c.style.margin = "0", c.style.top = o[b].top - parseFloat(c.style.marginTop) - t.top + v + "px", c.style.left = o[b].left - parseFloat(c.style.marginLeft) - t.left + u + "px"
+                });
+                var w = a(j).clone(),
+                    x = w.get(0);
+                x.innerHTML = "", x.setAttribute("id", ""), x.style.height = "auto", x.style.width = j.width() + "px", w.append(i), w.insertBefore(j), w.css("opacity", 0), x.style.zIndex = -1, x.style.margin = "0", x.style.position = "absolute", x.style.top = n.top - t.top + "px", x.style.left = n.left - t.left + "px", e.adjustHeight === "dynamic" ? j.animate({
+                    height: w.height()
+                }, e.duration, e.easing) : e.adjustHeight === "auto" && (l = w.height(), parseFloat(k) < parseFloat(l) ? j.css("height", l) : m = !0), p.each(function (b) {
+                    var c = [];
+                    typeof e.attribute == "function" ? (g = e.attribute(a(this)), i.each(function () {
+                        if (e.attribute(this) == g) {
+                            c = a(this);
+                            return !1
+                        }
+                    })) : c = i.filter("[" + e.attribute + "=" + a(this).attr(e.attribute) + "]"), c.length ? e.useScaling ? h.push({
+                        element: a(this),
+                        animation: {
+                            top: c.offset().top - t.top,
+                            left: c.offset().left - t.left,
+                            opacity: 1,
+                            scale: "1.0"
+                        }
+                    }) : h.push({
+                        element: a(this),
+                        animation: {
+                            top: c.offset().top - t.top,
+                            left: c.offset().left - t.left,
+                            opacity: 1
+                        }
+                    }) : e.useScaling ? h.push({
+                        element: a(this),
+                        animation: {
+                            opacity: "0.0",
+                            scale: "0.0"
+                        }
+                    }) : h.push({
+                        element: a(this),
+                        animation: {
+                            opacity: "0.0"
+                        }
+                    })
+                }), i.each(function (b) {
+                    var c = [],
+                        f = [];
+                    typeof e.attribute == "function" ? (g = e.attribute(a(this)), p.each(function () {
+                        if (e.attribute(this) == g) {
+                            c = a(this);
+                            return !1
+                        }
+                    }), i.each(function () {
+                        if (e.attribute(this) == g) {
+                            f = a(this);
+                            return !1
+                        }
+                    })) : (c = p.filter("[" + e.attribute + "=" + a(this).attr(e.attribute) + "]"), f = i.filter("[" + e.attribute + "=" + a(this).attr(e.attribute) + "]"));
+                    var k;
+                    if (c.length === 0) {
+                        e.useScaling ? k = {
+                            opacity: "1.0",
+                            scale: "1.0"
+                        } : k = {
+                            opacity: "1.0"
+                        }, d = f.clone();
+                        var l = d.get(0);
+                        l.style.position = "absolute", l.style.margin = "0", l.style.top = f.offset().top - t.top + "px", l.style.left = f.offset().left - t.left + "px", d.css("opacity", 0), e.useScaling && d.css("transform", "scale(0.0)"), d.appendTo(j), h.push({
+                            element: a(d),
+                            animation: k
+                        })
+                    }
+                }), w.remove(), e.enhancement(j);
+                for (c = 0; c < h.length; c++) h[c].element.animate(h[c].animation, e.duration, e.easing, r)
+            }
+        })
+    }
+})(jQuery);
 (function(){
 	var a = Backbone.Model.extend(),
 	b = Backbone.Collection.extend({
@@ -47,6 +164,42 @@
 	d = Backbone.Collection.extend({
 		model: c
 	}),
+	e = Backbone.Model.extend({
+            initialize: function () {
+                this.sects = new d
+            }
+        }, {
+            title: document.title,
+            txt2name: function (a) {
+                return $.trim(a).toLowerCase().replace(/\s+/g, "-")
+            }
+        }),
+	f = Backbone.Router.extend({
+            routes: {
+                "*path": "match"
+            },
+            redirects: {
+                "": "/"
+            },
+            match: function (a) {
+                a in this.redirects && (a = this.redirects[a]);
+                var b = {
+                    activePage: a
+                };
+                D.sects.some(function (a) {
+                    if (!a.validate(b)) {
+                        a.set(b, {
+                            silent: !0
+                        }), D.set({
+                            activeSect: a.id
+                        }, {
+                            silent: !0
+                        }), D.change(), a.change();
+                        return !0
+                    }
+                })
+            }
+        }),
 	g = function () {},
 	h = $(document),
 	i = [].slice,
@@ -57,8 +210,8 @@
 				var b = this.init;
 				this.init = function () {
 					var a = _.toArray(arguments),
-						c = a.pop();
-					//b.apply(this, a), this.init = g, c()
+						c = a.pop();						
+					b.apply(this, a), this.init = g;/*, c()*/
 				}
 			}
 			this.queue("init", "show", "hide")
@@ -75,7 +228,34 @@
 		init: g,
 		show: g,
 		hide: g
-	})
+	}),
+	l = k.extend({
+		activate: function (a, b) {
+			this.init();
+			var c = b.pages.get(b.get("activePage")),
+				d = c.view,
+				e = {
+					sectChanged: !! a
+				};
+			if (!b.hasChanged("activePage")) d.activate(null, c, e);
+			else {
+				var f = b.pages.get(b.previous("activePage")),
+					g = f.view;
+				g.deactivate(f, c, e), d.activate(f, c, e)
+			}
+			a && this.show()
+		},
+		deactivate: function (a, b) {
+			var c = a.pages.get(a.get("activePage")),
+				d = c.view;
+			this.hide(), d.deactivate(c, null)
+		},
+		show: function (a) {
+			/*$("#page").animate({
+				top: -this.model.id * 100 + "%"
+			}, a)*/
+		}
+	}),
 	m = k.extend({
 		initialize: function (a) {
 			k.prototype.initialize.apply(this, arguments), this.container = a.container || this.el, this.queue("setTitle", "loadImages", "scrollToTop")
@@ -154,23 +334,26 @@
 			var a = this.$(".thumbnails"),
 				b = _.template($("#filter-tpl").html(), {});
 			$(b).insertBefore(a);
-			/*var c = $(this.el),
+			var c = $(this.el),
 				d = c.css("display") == "none";
 			d && c.css("visibility", "hidden").show();
+			
 			var f = this.$(".filter").lava();
+			
 			d && c.hide().css("visibility", "visible");
 			var g = a.clone();
+			
 			$("li", f).click(function () {
 				var b = $(this),
 					c = "." + e.txt2name(b.text()),
 					d = g.children(b.index() == 0 ? "" : c);
-				/*a.quicksand(d, {
+				a.quicksand(d, {
 					adjustHeight: "dynamic",
 					attribute: function (a) {
 						return $("a", a).attr("href")
 					}
-				})
-			})*/
+				});
+			})
 		},
 		activate: function () {
 			this.changeClass(), m.prototype.activate.apply(this, arguments)
@@ -188,12 +371,8 @@
 		container: K,
 		el: $(">header, >footer", K)
 	});
-	// con khi kho
-	var a = this.$(".thumbnails"),
-				b = _.template($("#filter-tpl").html(), {});
-				console.log(b);
-				$(b).insertBefore(a);
 	L.init();
+	
 })(jQuery);
 (function(){
 	var root = this;
